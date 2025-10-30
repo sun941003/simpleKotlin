d17c4ba7954e0125defb1d2e660b8fadc8f225be
mcs <148678249+sun941003@users.noreply.github.com>
2025-10-30 17:31:43 +0900

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+package org.example
+
+import org.example.source.Sub1
+
+fun main() {
+    Sub1().hello()
+}
---
+++ src/main/kotlin/source/Sub1.kt
@@ -0,0 +1,5 @@
+package org.example.source
+
+class Sub1() {
+    fun hello() = println("hello from sub1")
+}
